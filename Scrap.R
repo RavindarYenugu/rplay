dbd386a8ebd91b314aaeb88968e23edc91bffb47
ryenugu@gmail.com <ryenugu@gmail.com>
2016-08-02 15:56:33 -0400

    Tet

--- Scrap.R
+++ Scrap.R
@@ -1,1 +1,8 @@
-plot(mpg ~ wt, data = mtcars, col=2)
+library(ggplot2)
+library(gridExtra)
+
+p1 <- ggplot()+geom_line(aes(x=1:10,y=1:10))
+p2 <- ggplot()+geom_line(aes(x=1:10,y=1:10))
+p3 <- ggplot()+geom_line(aes(x=1:10,y=1:10)) 
+p4 <- ggplot()+geom_line(aes(x=1:10,y=1:10))
+grid.arrange(p1, p2, p3, p4, ncol = 2, top = "Multiple Plots")
